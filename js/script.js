476add76dce601df0d4ea077424d8573cda4b8e5
bevzenj <brisk0110@gmail.com>
2021-07-25 22:48:58 +0300

    added section news and create slider

--- js/script.js
+++ js/script.js
@@ -6,10 +6,28 @@ burgerBtn.addEventListener('click', () => {
 } );
 
 $(document).ready(function(){
-    $('.slider').slick({
-        infinite: true
-    });
-    $('.single-item').slick();
+    $('.news-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        variableWidth: false,
+        responsive: [
+            {
+                breakpoint: 1320,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+      });
   });
 
   
